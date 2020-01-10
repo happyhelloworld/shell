b9018770d02c0fa615dc9f418dcbb2669671e4c4
root <nanhua.wang>
2020-01-10 13:05:56 +0800

    add dir and images

--- mysql.sh
+++ mysql.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
+## 2020-01-10 13:05
 
 $PWD/myssh_sftp_linux.sh 192.168.1.205 root r00t@111 cluster_01_mysql.sh cluster_0102_mysql.sh 22 192.168.1.206 root root cluster_02_mysql.sh cluster_0202_mysql.sh 22
